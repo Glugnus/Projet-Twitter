3aa8249164c2a18878a92d47ef367137ee8508b6
Glugnus <pernet.aurelien@gmail.com>
2026-03-14 13:18:00 +0100

    fix

--- environment/production.js
+++ environment/production.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   dbUrl:
     "mongodb+srv://Aurel:1234@cluster0.0qmbppq.mongodb.net/twitter?appName=Cluster0",
-  cert: path.join(__dirname, ""),
-  key: path.join(__dirname, ""),
+  cert: "/etc/letsencrypt/live/www.dyma-test-project.ovh/fullchain.pem",
+
+  key: "/etc/letsencrypt/live/www.dyma-test-project.ovh/privkey.pem",
 };

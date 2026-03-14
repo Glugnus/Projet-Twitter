ecadb9fba9227daf317a7e42e4f804cc27e966fa
Glugnus <pernet.aurelien@gmail.com>
2026-03-14 10:55:10 +0100

    first commit

---
+++ environment/production.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+module.exports = {
+  dbUrl:
+    "mongodb+srv://Aurel:1234@cluster0.0qmbppq.mongodb.net/twitter?appName=Cluster0",
+  cert: path.join(__dirname, ""),
+  key: path.join(__dirname, ""),
+};
---
+++ routes/users.routes.js
@@ -0,0 +1,13 @@
+const { ensureAuthenticated } = require("../config/guards.config");
+const router = require("express").Router();
+const {
+  signup,
+  signupForm,
+  uploadImage,
+} = require("../controllers/users.controller");
+
+router.get("/signup/form", signupForm);
+router.post("/signup", signup);
+router.post("/update/image", ensureAuthenticated, [uploadImage]);
+
+module.exports = router;
